d65f452d4943a0b9a0f3407bd007080ecc98673a
rubeans <rubensglf@gmail.com>
2023-01-03 16:35:47 -0300

    add a pop-up modal when the button is clicked

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const openModal = document.querySelector('.open-modal')
+const modal = document.querySelector('#modal')
+
+openModal.addEventListener('click', () => {
+    modal.showModal()
+})
+
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
