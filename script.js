3ffe24bb8fec7bfab99b5aeefc269f2d229e74d1
rubeans <rubensglf@gmail.com>
2023-01-04 14:06:58 -0300

    try to figure out how to add new books from array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// MODAL
+const modal = document.querySelector('#modal')
 const openModal = document.querySelector('.open-modal')
 const closeModal = document.querySelector('.close-modal')
-const modal = document.querySelector('#modal')
 
 openModal.addEventListener('click', () => {
     modal.showModal()
@@ -9,7 +10,22 @@ closeModal.addEventListener('click', () => {
     modal.close()
 })
 
+// BOOK CONTENT
+const booksContainer = document.querySelector('.books-container')
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
+const pages = document.querySelector('#pages')
+const isRead = document.querySelector('#isRead')
+const addBookBtn = document.querySelector('.add-book')
+const form = document.querySelector('.form')
 let myLibrary = [];
+let addBook;
+
+
+form.addEventListener('submit', () => {
+    addBookToLibrary()
+    form.reset()
+})
 
 class Book {
     constructor(title, author, pages, isRead) {
@@ -21,5 +37,9 @@ class Book {
 }
 
 function addBookToLibrary() {
-    // do stuff here
+    addBook = new Book(title.value, author.value, pages.value, isRead.checked)
+    myLibrary.push(addBook)
+    myLibrary.forEach(book => {
+        console.log(myLibrary)
+    })
 }
