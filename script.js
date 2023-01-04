324cde0959873bc0c9c1659ac2c39da2366de608
rubeans <rubensglf@gmail.com>
2023-01-04 10:09:07 -0300

    add a button to close modal

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const openModal = document.querySelector('.open-modal')
+const closeModal = document.querySelector('.close-modal')
 const modal = document.querySelector('#modal')
 
 openModal.addEventListener('click', () => {
     modal.showModal()
+})
+closeModal.addEventListener('click', () => {
+    modal.close()
 })
 
 let myLibrary = [];
