88eb1d6b0d2aa29ab56e55c68d56fd14aabbf58b
rubeans <rubensglf@gmail.com>
2023-01-05 00:52:51 -0300

    let users add how many books they want

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// MODAL
+// MODAL - OPEN AND CLOSE THE MODAL
 const modal = document.querySelector('#modal')
 const openModal = document.querySelector('.open-modal')
 const closeModal = document.querySelector('.close-modal')
@@ -21,7 +21,7 @@ const form = document.querySelector('.form')
 let myLibrary = [];
 let newBook;
 
-
+// EVERYTIME FORM IS SUBMITTED IT WILL ADD A NEW BOOK AND RESET THE INPUT FIELDS
 form.addEventListener('submit', () => {
     addBookToLibrary()
     form.reset()
@@ -29,23 +29,44 @@ form.addEventListener('submit', () => {
 
 class Book {
     constructor(title, author, pages, isRead) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.isRead = isRead;
+        this.title = title.value;
+        this.author = author.value;
+        this.pages = pages.value;
+        this.isRead = isRead.checked;
     }
 }
 
 function addBookToLibrary() {
-    newBook = new Book(title.value, author.value, pages.value, isRead.checked)
+    newBook = new Book(title, author, pages, isRead)
     myLibrary.push(newBook)
     displayBook()
+    // console.log(myLibrary)
 }
 
+// DISPLAY THE BOOKS ON SCREEN
 function displayBook() {
+    // CREATE NEW ELEMENTS TO GIVE IT CHILDS
     const bookBox = document.createElement('div')
+    const bookInfo = document.createElement('div')
+    const bookBtns = document.createElement('div')
+    //BOOK INFO 
+    bookInfo.insertAdjacentHTML('beforeend', `
+        <span>"${newBook.title}"</span>
+        <span>By: ${newBook.author}</span>
+        <span>${newBook.pages} Pages</span>
+        `)
+    //BOOK BUTTONS
+    bookBtns.insertAdjacentHTML('beforeend', `
+        <button class="isRead">Read</button>
+        <button class="delete-btn">Delete</button>
+    `)
     myLibrary.forEach(book => {
+        // APPEND DIV CHILDS AND GIVE IT A CLASS
         bookBox.classList.add('book-box')
-        booksContainer.appendChild(bookBox)
+        booksContainer.append(bookBox)
+        bookInfo.classList.add('book-info')
+        bookBox.append(bookInfo)
+        bookBtns.classList.add('book-btns')
+        bookBox.append(bookBtns)
     })
 }
